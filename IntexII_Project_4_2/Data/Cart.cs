78e88aed187fbcfa14b58b418f55221c6f2f9c71
Brechristian <breshearschristian@hotmail.com>
2024-04-10 09:29:53 -0600

    created cart.cs and changed the code from water project "Project" to "Product"

---
+++ IntexII_Project_4_2/Data/Cart.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace IntexII_Project_4_2.Data
+{
+    public class Cart
+    {
+        public List<CartLine> Lines { get; set; } = new List<CartLine>();
+
+        public void AddItem(Product prod, int quantity)
+        {
+            CartLine? line = Lines
+                .Where(x => x.Product.ProductId == prod.ProductId)
+                .FirstOrDefault();
+
+            if (line == null)
+            {
+                Lines.Add(new CartLine
+                {
+                    Product = prod,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveLine(Product prod) => Lines.RemoveAll(x => x.Product.ProductId == prod.ProductId);
+        public void Clear() => Lines.Clear();
+        public decimal CalculateTotal() => Lines.Sum(x => 25 * x.Quantity);
+        public class CartLine
+        {
+            public int CartLineId { get; set; }
+            public Product Product { get; set; }
+            public int Quantity { get; set; }
+
+        }
+    }
+}
