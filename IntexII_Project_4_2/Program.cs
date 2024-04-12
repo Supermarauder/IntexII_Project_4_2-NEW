3ecdf67cbccaa08b33f833cd95143287cf63695d
Supermarauder <119464479+Supermarauder@users.noreply.github.com>
2024-04-12 09:47:11 -0600

    CSP HEADER IS BACK

--- IntexII_Project_4_2/Program.cs
+++ IntexII_Project_4_2/Program.cs
@@ -103,6 +103,45 @@ namespace IntexII_Project_4_2
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+            app.Use(async (ctx, next) =>
+            {
+                ctx.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+                ctx.Response.Headers.Add("X-XSS-Protection", "1; mode=block");
+                ctx.Response.Headers.Add("Referrer-Policy", "no-referrer");
+
+                // Updated Content-Security-Policy
+                var csp = "default-src 'self'; " +
+                          "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://app.termly.io https://apis.google.com https://accounts.google.com; " + // Allow scripts from Google APIs and Google Accounts
+                          "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " + // Allow styles from Google Fonts
+                          "img-src 'self' https://images.brickset.com https://www.lego.com https://*.amazonaws.com https://*.googleusercontent.com https://m.media-amazon.com https://www.brickeconomy.com data:; " + // Specify sources for images
+                          "font-src 'self' https://fonts.gstatic.com; " + // Allow fonts from Google Fonts
+                          "frame-src 'self' https://accounts.google.com; " + // Allow iframes from Google Accounts for OAuth
+                          "object-src 'none'; " + // Disallow all objects
+                          "base-uri 'self'; " +
+                          "form-action 'self' https://accounts.google.com; " + // Allow form actions to self and Google Accounts
+                          "connect-src 'self' https://accounts.google.com; " + // Allow connections to Google Accounts
+                          "frame-ancestors 'self';"; // Restrict who can frame the page
+
+                // Check if the current request is for the Google OAuth callback
+                if (ctx.Request.Path.StartsWithSegments(new PathString("/signin-google")))
+                {
+                    // Modify the CSP to allow all sources for the OAuth callback
+                    csp = "default-src * 'unsafe-inline' 'unsafe-eval';" +
+                          "script-src * 'unsafe-inline' 'unsafe-eval';" +
+                          "style-src * 'unsafe-inline';" +
+                          "img-src * data:;" +
+                          "font-src *;" +
+                          "frame-src *;" +
+                          "object-src *;" +
+                          "base-uri 'self';" +
+                          "form-action *;" +
+                          "connect-src *;" +
+                          "frame-ancestors *;";
+                }
+
+                ctx.Response.Headers.Add("Content-Security-Policy", csp);
+                await next();
+            });
 
             app.UseHsts();
             app.UseHttpsRedirection();
